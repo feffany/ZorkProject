7b396fb6988a78925d108f602b66ce2802f0fba9
feffany <48835773+feffany@users.noreply.github.com>
2020-10-01 19:57:27 -0400

    Zork 2.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -26,11 +26,10 @@ namespace Zork
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                //string outputString;
                 switch(command)
                 {
                     case Commands.LOOK:
-                        Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        Console.WriteLine("A rubber mat saying 'Welcome to Zork!' lies by the door.");
                         break;
 
                     case Commands.NORTH:
@@ -55,43 +54,11 @@ namespace Zork
                         Console.WriteLine("Unrecognized command.");
                         break;
                 }
-
-                //Console.WriteLine(outputString);
-            }
-
-            //1.2
-            /*string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);*/
-
-            //1.1
-            /*inputString = inputString.ToUpper();
-
-            if(inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if(inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork! lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }*/
-        }
-
-        private static Commands ToCommand(string commandString)
-        {
-            if(Enum.TryParse<Commands>(commandString, true, out Commands result))
-            {
-                return result;
-            }
-            else
-            {
-                return Commands.UNKNOWN;
             }
         }
+
+        private static Commands ToCommand(string commandString) =>
+            Enum.TryParse(commandString, true, out Commands result) ?result : Commands.UNKNOWN;
 
         private static bool Move(Commands movement)
         {
